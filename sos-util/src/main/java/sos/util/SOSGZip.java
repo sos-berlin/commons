74497aa37a9be8bd75a1d5ae6647f2d7c244fe8a
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2014-09-07 20:03:34 +0000

    -
    svn-id 27111

--- sos-util/src/main/java/sos/util/SOSGZip.java
+++ sos-util/src/main/java/sos/util/SOSGZip.java
@@ -1,13 +1,12 @@
 package sos.util;
 
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
 import java.io.BufferedInputStream;
 import java.io.File;
-import java.util.zip.GZIPOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * <p>Title: </p>
@@ -18,119 +17,132 @@ import java.util.zip.GZIPInputStream;
  * @version $Id$
  */
 
-
 public class SOSGZip {
-	
+
 	/**
 	 * @param inputFile
 	 * @param outputFile
 	 * @exception FileNotFoundException, IOException
 	 */
-	public static void compressFile(
-			File inputFile, File outputFile ) throws
-			Exception {
-		
-		
+	public static void compressFile(File inputFile, File outputFile) throws Exception {
+
 		BufferedInputStream in = null;
 		GZIPOutputStream out = null;
-		
+
 		try {
-			
-			in = new BufferedInputStream( new FileInputStream(inputFile));
-			
-			out = new GZIPOutputStream( new FileOutputStream(outputFile));
-			
+			in = new BufferedInputStream(new FileInputStream(inputFile));
+			out = new GZIPOutputStream(new FileOutputStream(outputFile));
+
 			byte buffer[] = new byte[1024];
 			int bytesRead;
-			while ( (bytesRead = in.read(buffer)) != -1) {
+			while ((bytesRead = in.read(buffer)) != -1) {
 				out.write(buffer, 0, bytesRead);
 			}
-			
-		}  catch (Exception e) {
+
+		}
+		catch (Exception e) {
 			throw e;
-		} finally {
-			try { in.close(); } catch( Exception e) {}
-			try { out.close(); } catch( Exception e) {}
+		}
+		finally {
+			try {
+				in.close();
+			}
+			catch (Exception e) {
+			}
+			try {
+				out.flush();
+				out.close();
+			}
+			catch (Exception e) {
+			}
 		}
 	}
-	
-	
+
 	/**
 	 * @param fileInputStream
 	 * @param outputFile
 	 * @exception FileNotFoundException, IOException
 	 */
-	public static void compressFile(
-			FileInputStream fileInputStream, File outputFile ) throws
-			Exception {
-		
+	public static void compressFile(FileInputStream fileInputStream, File outputFile) throws Exception {
+
 		BufferedInputStream in = null;
 		GZIPOutputStream out = null;
 		try {
-			
+
 			in = new BufferedInputStream(fileInputStream);
-			
-			out = new GZIPOutputStream( new FileOutputStream(outputFile));
-			
+
+			out = new GZIPOutputStream(new FileOutputStream(outputFile));
+
 			byte buffer[] = new byte[1024];
 			int bytesRead;
-			while ( (bytesRead = in.read(buffer)) != -1) {
+			while ((bytesRead = in.read(buffer)) != -1) {
 				out.write(buffer, 0, bytesRead);
 			}
-		}  catch (Exception e) {
+		}
+		catch (Exception e) {
 			throw e;
-		} finally {
-			try { in.close(); } catch( Exception e) {}
-			try { out.close(); } catch( Exception e) {}
+		}
+		finally {
+			try {
+				in.close();
+			}
+			catch (Exception e) {
+			}
+			try {
+				out.close();
+			}
+			catch (Exception e) {
+			}
 		}
 	}
-	
+
 	/**
 	 * Decomprimiert die Archivdatei
 	 * @param inFilename
 	 * @throws Exception
 	 */
-	public static void decompress(String inFilename) throws	Exception{
+	public static void decompress(String inFilename) throws Exception {
 		decompress(inFilename, null);
 	}
-	
+
 	/**
 	 * Decomprimiert die Archivdatei
 	 * @param inFilename
 	 * @param outFilename
 	 * @throws Exception
 	 */
-	public static void decompress(String inFilename, String outFilename) throws	Exception{
+	public static void decompress(String inFilename, String outFilename) throws Exception {
 		try {
-			if(outFilename == null) {
+			if (outFilename == null) {
 				outFilename = inFilename.substring(0, inFilename.lastIndexOf("."));
 			}
-			
+
 			GZIPInputStream in = new GZIPInputStream(new FileInputStream(inFilename));
-			
+
 			java.io.OutputStream out = new FileOutputStream(outFilename);
-			
-			
+
 			byte[] buf = new byte[1024];
 			int len;
 			while ((len = in.read(buf)) > 0) {
 				out.write(buf, 0, len);
 			}
-			
+
 			in.close();
 			out.close();
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			throw e;
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			compressFile(new File("C:/temp/a.txt"), new File("C:/temp/a.gz"));
-			
+
 			//decompress("C:/temp/a/RDN_20070511_000001.tar.gz", null);
-		} catch(Exception e) {
+		}
+		catch (Exception e) {
 			System.out.println(e);
 		}
 	}
