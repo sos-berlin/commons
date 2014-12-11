5f0ad917318e147a43f8f1b5d8061dd19f7a48e6
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2014-12-11 22:44:16 +0100

    back to SOSProductionResource from 1.8 otherwise compile error in /joe-xml/.../DomParser,java

--- common-resources/src/main/java/com/sos/resources/SOSProductionResource.java
+++ common-resources/src/main/java/com/sos/resources/SOSProductionResource.java
@@ -1,5 +1,11 @@
 package com.sos.resources;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.transform.stream.StreamSource;
+
 /**
  * A list of common resources used in different products/projects.
  *
@@ -9,7 +15,10 @@ public enum SOSProductionResource implements SOSResource {
 
     JOB_CHAIN_EXTENSIONS_XSD(basePackage() + "/xsd","job-chain-extensions-v1.0.xsd"),
 
-    JOB_DOC_XSD(basePackage() + "/xsd","scheduler_job_documentation_v1.1.xsd"),
+    JOB_DOC_XSD(basePackage() + "/xsd","scheduler_job_documentation_v1.2.xsd"),
+    JOB_DOC_XSLT(basePackage() + "/xsl/jobdoc","scheduler_job_documentation_v1.1.xsl"),
+    // ist zwar die höhere Versionsnummer aber leider wurde 1.1 weitergepflegt :-(
+//    JOB_DOC_XSLT(basePackage() + "/xsl","scheduler_job_documentation_v2.0.xsl"),
 
     EVENT_SERVICE_XSD(basePackage() + "/xsd","events2actions.xsd"),
 
@@ -28,7 +37,32 @@ public enum SOSProductionResource implements SOSResource {
         return SOSProductionResource.class.getPackage().getName().replaceAll("\\.","/");
     }
 
-    public String getFullName() {
+    @Override public String getFullName() {
         return packageName + resourceName;
     }
+
+    public  StreamSource getAsStreamSource () {
+    	return new StreamSource(getInputStream4Resource());
+    }
+    
+	public  InputStream getInputStream4Resource () {
+		String pstrKey = "/" + getFullName();  // in jar it works with leading slash only
+		InputStream objSS = this.getClass().getResourceAsStream(pstrKey);
+		if (objSS == null) {
+			throw new RuntimeException("Resource not found: " + pstrKey);
+		}
+		return objSS;
+	}
+
+    public URL getURL() {
+    	URL objU = null;
+		try {
+			objU = new URL(packageName + resourceName);
+		}
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+        return objU;
+    }
+
 }
