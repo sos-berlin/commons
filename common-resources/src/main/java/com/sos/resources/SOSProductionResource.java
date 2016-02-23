842ce5a493779f31345dcb4988bca59676b78711
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2016-02-23 15:36:22 +0100

    JITL-241, JITL-246

--- common-resources/src/main/java/com/sos/resources/SOSProductionResource.java
+++ common-resources/src/main/java/com/sos/resources/SOSProductionResource.java
@@ -6,25 +6,23 @@ import java.net.URL;
 
 import javax.xml.transform.stream.StreamSource;
 
-/**
- * A list of common resources used in different products/projects.
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/** A list of common resources used in different products/projects.
  *
- * The list entries should refer to the newest version of a resource file.
- */
+ * The list entries should refer to the newest version of a resource file. */
 public enum SOSProductionResource implements SOSResource {
 
-    JOB_CHAIN_EXTENSIONS_XSD(basePackage() + "/xsd","job-chain-extensions-v1.0.xsd"),
-
-    JOB_DOC_XSD(basePackage() + "/xsd","scheduler_job_documentation_v1.2.xsd"),
-    JOB_DOC_XSLT(basePackage() + "/xsl/jobdoc","scheduler_job_documentation_v1.1.xsl"),
-    // ist zwar die höhere Versionsnummer aber leider wurde 1.1 weitergepflegt :-(
-//    JOB_DOC_XSLT(basePackage() + "/xsl","scheduler_job_documentation_v2.0.xsl"),
-
-    EVENT_SERVICE_XSD(basePackage() + "/xsd","events2actions.xsd"),
-
+    JOB_CHAIN_EXTENSIONS_XSD(basePackage() + "/xsd", "job-chain-extensions-v1.0.xsd"),
+    JOB_DOC_XSD(basePackage() + "/xsd", "scheduler_job_documentation_v1.2.xsd"),
+    JOB_DOC_XSLT(basePackage() + "/xsl/jobdoc", "scheduler_job_documentation_v1.1.xsl"),
+    EVENT_SERVICE_XSD(basePackage() + "/xsd", "events2actions.xsd"),
     // the version is determined via the dependency setting in the pom.xml
-    SCHEDULER_XSD("com/sos/scheduler/enginedoc/common","scheduler.xsd");
+    SCHEDULER_XSD("com/sos/scheduler/enginedoc/common", "scheduler.xsd");
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SOSProductionResource.class);
     private final String packageName;
     private final String resourceName;
 
@@ -34,34 +32,34 @@ public enum SOSProductionResource implements SOSResource {
     }
 
     public static String basePackage() {
-        return SOSProductionResource.class.getPackage().getName().replaceAll("\\.","/");
+        return SOSProductionResource.class.getPackage().getName().replaceAll("\\.", "/");
     }
 
-    @Override public String getFullName() {
+    @Override
+    public String getFullName() {
         return packageName + resourceName;
     }
 
-    public  StreamSource getAsStreamSource () {
-    	return new StreamSource(getInputStream4Resource());
+    public StreamSource getAsStreamSource() {
+        return new StreamSource(getInputStream4Resource());
     }
-    
-	public  InputStream getInputStream4Resource () {
-		String pstrKey = "/" + getFullName();  // in jar it works with leading slash only
-		InputStream objSS = this.getClass().getResourceAsStream(pstrKey);
-		if (objSS == null) {
-			throw new RuntimeException("Resource not found: " + pstrKey);
-		}
-		return objSS;
-	}
+
+    public InputStream getInputStream4Resource() {
+        String pstrKey = "/" + getFullName();
+        InputStream objSS = this.getClass().getResourceAsStream(pstrKey);
+        if (objSS == null) {
+            throw new RuntimeException("Resource not found: " + pstrKey);
+        }
+        return objSS;
+    }
 
     public URL getURL() {
-    	URL objU = null;
-		try {
-			objU = new URL(packageName + resourceName);
-		}
-		catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
+        URL objU = null;
+        try {
+            objU = new URL(packageName + resourceName);
+        } catch (MalformedURLException e) {
+            LOGGER.error("", e);
+        }
         return objU;
     }
 
