d3d38f05f19560becf87ceaa230946fd50f19002
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2016-03-02 15:57:19 +0100

    JITL-241

--- sos-util/src/main/java/sos/util/SOSBufferedLogger.java
+++ sos-util/src/main/java/sos/util/SOSBufferedLogger.java
@@ -4,9 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/** Logger which logs into a Buffer
- *
- * @author Andreas Liebert */
+/** @author Andreas Liebert */
 public class SOSBufferedLogger extends SOSLogger {
 
     protected StringBuffer logBuffer = new StringBuffer();
@@ -17,26 +15,15 @@ public class SOSBufferedLogger extends SOSLogger {
     private Date currentDate = new Date();
     private int logLevel = 10;
 
-    /** Konstruktor logging nach buffer
-     * 
-     * @param logLevel ein Wert von 1 bis 9
-     * @throws Exception */
     public SOSBufferedLogger(int logLevel) throws Exception {
         this.setLogLevel(logLevel);
         writeHeader();
     }
 
-    /** Schreibt den header
-     * 
-     * @throws java.lang.Exception */
     private void writeHeader() throws Exception {
         logBuffer.append(TIME.format(currentDate) + INDENT + "---------- " + TIMESTAMP.format(currentDate) + nl);
     }
 
-    /** Loggt die angegebene message
-     * 
-     * @param level Protokollierungstiefe
-     * @param str stellt die log message dar */
     private void log(int level, String str) {
         if (level > logLevel && level < INFO) {
             return;
@@ -70,150 +57,80 @@ public class SOSBufferedLogger extends SOSLogger {
         logBuffer.append(curBuffer);
     }
 
-    /** Schreibt eine Warnung ins Protokoll
-     * 
-     * @param warningMessage Inhalt der Warnung
-     * @throws java.lang.Exception */
     public void warn(String warningMessage) throws Exception {
         log(WARN, warningMessage);
         setWarning(warningMessage);
     }
 
-    /** Schreibt eine Fehlermeldung ins Protokoll
-     * 
-     * @param errorMessage Inhalt der Fehlermeldung
-     * @throws java.lang.Exception */
     public void error(String errorMessage) throws Exception {
         log(ERROR, errorMessage);
         setError(errorMessage);
     }
 
-    /** Schreibt info-message
-     * 
-     * @param str stellt die info message dar
-     * @throws java.lang.Exception */
     public void info(String str) throws Exception {
         log(INFO, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug(String str) throws Exception {
         log(DEBUG, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug1(String str) throws Exception {
         log(DEBUG1, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug2(String str) throws Exception {
         log(DEBUG2, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug3(String str) throws Exception {
         log(DEBUG3, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug4(String str) throws Exception {
         log(DEBUG4, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug5(String str) throws Exception {
         log(DEBUG5, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug6(String str) throws Exception {
         log(DEBUG6, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug7(String str) throws Exception {
         log(DEBUG7, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug8(String str) throws Exception {
         log(DEBUG8, str);
     }
 
-    /** Schreibt debug-message
-     * 
-     * @param str stellt die debug message dar
-     * @throws java.lang.Exception */
     public void debug9(String str) throws Exception {
         log(DEBUG9, str);
     }
 
-    /** Schliesst das stream-objekt
-     * 
-     * @throws java.lang.Exception */
     public void close() throws Exception {
         this.reset();
         this.logBuffer = null;
     }
 
-    /** Destruktor
-     * 
-     * @throws Exception */
-    protected void finalize() throws Exception {
-        try {
-            if (this.logBuffer != null){
-                this.close();
-            }
-        } catch (Exception e) {
-        }
-    }
-
-    /** setzt den Zustand des Loggers zurück */
     public void reset() {
         super.reset();
         this.logBuffer = new StringBuffer();
     }
 
-    /** @return Returns the logLevel. */
     public int getLogLevel() {
         return logLevel;
     }
 
-    /** @param logLevel The logLevel to set. */
     public void setLogLevel(int logLevel) {
         this.logLevel = logLevel;
     }
 
-    /** @return Returns the logBuffer. */
     public StringBuffer getLogBuffer() {
         return logBuffer;
     }
+    
 }
