ab2377a832cfa67a9651898255157e5cf0d448f6
Uwe Risse <uwe.risse-git@sos-berlin.com>
2017-12-13 15:33:14 +0100

    JOC-256

---
+++ sos-json/src/main/java/com/sos/classes/CustomEventsUtil.java
@@ -0,0 +1,58 @@
+package com.sos.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sos.jobscheduler.model.event.CalendarObjectType;
+import com.sos.jobscheduler.model.event.CalendarVariables;
+import com.sos.jobscheduler.model.event.CustomEvent;
+import com.sos.jobscheduler.model.event.CustomEventVariables;
+
+public class CustomEventsUtil {
+
+    private ObjectMapper objectMapper;
+    private List<CustomEvent> listOfCustomEvents;
+    private String source;
+
+    public CustomEventsUtil(String source) {
+        super();
+        objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+        listOfCustomEvents = new ArrayList<CustomEvent>();
+    }
+
+    public String getEventCommandAsXml() throws JsonProcessingException {
+        String xmlCommand = "";
+        if (!listOfCustomEvents.isEmpty()) {
+
+            if (listOfCustomEvents.size() > 1) {
+                xmlCommand += "<commands>";
+            }
+            for (CustomEvent event : listOfCustomEvents) {
+                String command = getEventAsCommand(event);
+                xmlCommand += command;
+            }
+            if (listOfCustomEvents.size() > 1) {
+                xmlCommand += "</commands>";
+            }
+        }
+        return xmlCommand;
+    }
+
+    public void addEvent(String key) throws JsonProcessingException {
+        CustomEvent customEvent = new CustomEvent();
+        customEvent.setKey(key);
+        CustomEventVariables customEventVariables = new CustomEventVariables();
+        customEventVariables.setSource(source);
+        customEvent.setVariables(customEventVariables);
+        listOfCustomEvents.add(customEvent);
+    }
+
+    private String getEventAsCommand(CustomEvent customEvent) throws JsonProcessingException {
+        String xmlCommand = objectMapper.writeValueAsString(customEvent);
+        return "<publish_event>" + xmlCommand + "</publish_event>";
+    }
+
+}
