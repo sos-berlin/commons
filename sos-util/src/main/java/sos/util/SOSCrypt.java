bc51fb724431fca04deb9e9e57c9d64f8213a907
Uwe Risse <uwe.risse-git@sos-berlin.com>
2018-04-17 17:21:23 +0200

    JOE-275

--- sos-util/src/main/java/sos/util/SOSCrypt.java
+++ sos-util/src/main/java/sos/util/SOSCrypt.java
@@ -4,51 +4,61 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.MessageDigest;
-
+import java.util.Base64;
 
 /** @author Ghassan Beydoun */
 public class SOSCrypt {
 
-    public static String md5encrypt(String str) throws Exception {
-        MessageDigest md = null;
-        md = MessageDigest.getInstance("MD5");
-        md.reset();
-        md.update(str.getBytes());
-        return toHexString(md.digest());
-    }
+	public static String encrypt(String text, String algName) throws Exception {
+		MessageDigest md = null;
+		Base64.Encoder encoder = java.util.Base64.getMimeEncoder();
 
-    public static String md5encrypt(File inputFile) throws Exception {
-        byte[] b = createChecksum(inputFile);
-        String result = toHexString(b);
-        return result;
-    }
+		md = MessageDigest.getInstance(algName);
+		md.reset();
+		md.update(text.getBytes());
+		return encoder.encodeToString(md.digest());
+	}
 
-    public static byte[] createChecksum(File inputFile) throws Exception {
-        InputStream fis = new FileInputStream(inputFile);
-        byte[] buffer = new byte[1024];
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        int numRead;
-        do {
-            numRead = fis.read(buffer);
-            if (numRead > 0) {
-                md.update(buffer, 0, numRead);
-            }
-        } while (numRead != -1);
-        fis.close();
-        return md.digest();
-    }
+	public static String md5encrypt(String str) throws Exception {
+		MessageDigest md = null;
+		md = MessageDigest.getInstance("MD5");
+		md.reset();
+		md.update(str.getBytes());
+		return toHexString(md.digest());
+	}
 
-    private static String toHexString(byte[] b) {
-        char[] hexChar = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-        int length = b.length * 2;
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < b.length; i++) {
-            // oberer Byteanteil
-            sb.append(hexChar[(b[i] & 0xf0) >>> 4]);
-            // unterer Byteanteil
-            sb.append(hexChar[b[i] & 0x0f]);
-        }
-        return sb.toString();
-    }
+	public static String md5encrypt(File inputFile) throws Exception {
+		byte[] b = createChecksum(inputFile);
+		String result = toHexString(b);
+		return result;
+	}
+
+	public static byte[] createChecksum(File inputFile) throws Exception {
+		InputStream fis = new FileInputStream(inputFile);
+		byte[] buffer = new byte[1024];
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		int numRead;
+		do {
+			numRead = fis.read(buffer);
+			if (numRead > 0) {
+				md.update(buffer, 0, numRead);
+			}
+		} while (numRead != -1);
+		fis.close();
+		return md.digest();
+	}
+
+	private static String toHexString(byte[] b) {
+		char[] hexChar = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		int length = b.length * 2;
+		StringBuilder sb = new StringBuilder(length);
+		for (int i = 0; i < b.length; i++) {
+			// oberer Byteanteil
+			sb.append(hexChar[(b[i] & 0xf0) >>> 4]);
+			// unterer Byteanteil
+			sb.append(hexChar[b[i] & 0x0f]);
+		}
+		return sb.toString();
+	}
 
 }
