6add51c07e416de75d1fdbfcfaf7f9eb82383f39
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2018-08-07 16:19:39 +0200

    JOC-462

--- sos-json/src/main/java/com/sos/joc/model/job/JobsFilter.java
+++ sos-json/src/main/java/com/sos/joc/model/job/JobsFilter.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.sos.joc.model.common.Folder;
 import com.sos.joc.model.common.HistoryStateText;
-import com.sos.joc.model.order.OrderPath;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
