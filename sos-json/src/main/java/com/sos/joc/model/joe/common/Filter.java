a1727e4e1c92b4a17cd09c6aa03bfd307a5a8aae
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2019-09-25 14:06:44 +0200

    JOC-730

---
+++ sos-json/src/main/java/com/sos/joc/model/joe/common/Filter.java
@@ -0,0 +1,224 @@
+
+package com.sos.joc.model.joe.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.sos.joc.model.audit.AuditParams;
+import com.sos.joc.model.common.JobSchedulerObjectType;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * filter for requests
+ * <p>
+ * oldPath is used for a move/rename, auditLog only for deploy
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JacksonXmlRootElement(localName = "filter")
+@JsonPropertyOrder({
+    "jobschedulerId",
+    "path",
+    "oldPath",
+    "objectType",
+    "auditLog"
+})
+public class Filter {
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("jobschedulerId")
+    @JacksonXmlProperty(localName = "jobscheduler_id", isAttribute = true)
+    private String jobschedulerId;
+    /**
+     * path
+     * <p>
+     * absolute path based on live folder of a JobScheduler object.
+     * (Required)
+     * 
+     */
+    @JsonProperty("path")
+    @JsonPropertyDescription("absolute path based on live folder of a JobScheduler object.")
+    @JacksonXmlProperty(localName = "path", isAttribute = true)
+    private String path;
+    /**
+     * path
+     * <p>
+     * absolute path based on live folder of a JobScheduler object.
+     * 
+     */
+    @JsonProperty("oldPath")
+    @JsonPropertyDescription("absolute path based on live folder of a JobScheduler object.")
+    @JacksonXmlProperty(localName = "old_path", isAttribute = true)
+    private String oldPath;
+    /**
+     * JobScheduler object type
+     * <p>
+     * 
+     * 
+     */
+    @JsonProperty("objectType")
+    @JacksonXmlProperty(localName = "object_type", isAttribute = false)
+    private JobSchedulerObjectType objectType;
+    /**
+     * auditParams
+     * <p>
+     * 
+     * 
+     */
+    @JsonProperty("auditLog")
+    @JacksonXmlProperty(localName = "audit_log", isAttribute = false)
+    private AuditParams auditLog;
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("jobschedulerId")
+    @JacksonXmlProperty(localName = "jobscheduler_id", isAttribute = true)
+    public String getJobschedulerId() {
+        return jobschedulerId;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("jobschedulerId")
+    @JacksonXmlProperty(localName = "jobscheduler_id", isAttribute = true)
+    public void setJobschedulerId(String jobschedulerId) {
+        this.jobschedulerId = jobschedulerId;
+    }
+
+    /**
+     * path
+     * <p>
+     * absolute path based on live folder of a JobScheduler object.
+     * (Required)
+     * 
+     */
+    @JsonProperty("path")
+    @JacksonXmlProperty(localName = "path", isAttribute = true)
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * path
+     * <p>
+     * absolute path based on live folder of a JobScheduler object.
+     * (Required)
+     * 
+     */
+    @JsonProperty("path")
+    @JacksonXmlProperty(localName = "path", isAttribute = true)
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * path
+     * <p>
+     * absolute path based on live folder of a JobScheduler object.
+     * 
+     */
+    @JsonProperty("oldPath")
+    @JacksonXmlProperty(localName = "old_path", isAttribute = true)
+    public String getOldPath() {
+        return oldPath;
+    }
+
+    /**
+     * path
+     * <p>
+     * absolute path based on live folder of a JobScheduler object.
+     * 
+     */
+    @JsonProperty("oldPath")
+    @JacksonXmlProperty(localName = "old_path", isAttribute = true)
+    public void setOldPath(String oldPath) {
+        this.oldPath = oldPath;
+    }
+
+    /**
+     * JobScheduler object type
+     * <p>
+     * 
+     * 
+     */
+    @JsonProperty("objectType")
+    @JacksonXmlProperty(localName = "object_type", isAttribute = false)
+    public JobSchedulerObjectType getObjectType() {
+        return objectType;
+    }
+
+    /**
+     * JobScheduler object type
+     * <p>
+     * 
+     * 
+     */
+    @JsonProperty("objectType")
+    @JacksonXmlProperty(localName = "object_type", isAttribute = false)
+    public void setObjectType(JobSchedulerObjectType objectType) {
+        this.objectType = objectType;
+    }
+
+    /**
+     * auditParams
+     * <p>
+     * 
+     * 
+     */
+    @JsonProperty("auditLog")
+    @JacksonXmlProperty(localName = "audit_log", isAttribute = false)
+    public AuditParams getAuditLog() {
+        return auditLog;
+    }
+
+    /**
+     * auditParams
+     * <p>
+     * 
+     * 
+     */
+    @JsonProperty("auditLog")
+    @JacksonXmlProperty(localName = "audit_log", isAttribute = false)
+    public void setAuditLog(AuditParams auditLog) {
+        this.auditLog = auditLog;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("jobschedulerId", jobschedulerId).append("path", path).append("oldPath", oldPath).append("objectType", objectType).append("auditLog", auditLog).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(path).append(oldPath).append(jobschedulerId).append(auditLog).append(objectType).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Filter) == false) {
+            return false;
+        }
+        Filter rhs = ((Filter) other);
+        return new EqualsBuilder().append(path, rhs.path).append(oldPath, rhs.oldPath).append(jobschedulerId, rhs.jobschedulerId).append(auditLog, rhs.auditLog).append(objectType, rhs.objectType).isEquals();
+    }
+
+}
