a9cac9b766161a46840be47372d10562717d4419
SOS-RE <robert.ehrlich@sos-berlin.com>
2019-12-06 09:39:05 +0100

    JOC-797

---
+++ sos-json/src/main/java/com/sos/joc/model/xmleditor/xml2json/Xml2JsonConfiguration.java
@@ -0,0 +1,150 @@
+
+package com.sos.joc.model.xmleditor.xml2json;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.sos.joc.model.xmleditor.common.ObjectType;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * xmleditor xml2json configuration in
+ * <p>
+ * schema only for OTHER
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "jobschedulerId",
+    "objectType",
+    "configuration",
+    "schema"
+})
+public class Xml2JsonConfiguration {
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("jobschedulerId")
+    private String jobschedulerId;
+    /**
+     * xmleditor object type
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("objectType")
+    private ObjectType objectType;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("configuration")
+    private String configuration;
+    @JsonProperty("schema")
+    private String schema;
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("jobschedulerId")
+    public String getJobschedulerId() {
+        return jobschedulerId;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("jobschedulerId")
+    public void setJobschedulerId(String jobschedulerId) {
+        this.jobschedulerId = jobschedulerId;
+    }
+
+    /**
+     * xmleditor object type
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("objectType")
+    public ObjectType getObjectType() {
+        return objectType;
+    }
+
+    /**
+     * xmleditor object type
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("objectType")
+    public void setObjectType(ObjectType objectType) {
+        this.objectType = objectType;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("configuration")
+    public String getConfiguration() {
+        return configuration;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("configuration")
+    public void setConfiguration(String configuration) {
+        this.configuration = configuration;
+    }
+
+    @JsonProperty("schema")
+    public String getSchema() {
+        return schema;
+    }
+
+    @JsonProperty("schema")
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("jobschedulerId", jobschedulerId).append("objectType", objectType).append("configuration", configuration).append("schema", schema).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(schema).append(jobschedulerId).append(configuration).append(objectType).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Xml2JsonConfiguration) == false) {
+            return false;
+        }
+        Xml2JsonConfiguration rhs = ((Xml2JsonConfiguration) other);
+        return new EqualsBuilder().append(schema, rhs.schema).append(jobschedulerId, rhs.jobschedulerId).append(configuration, rhs.configuration).append(objectType, rhs.objectType).isEquals();
+    }
+
+}
