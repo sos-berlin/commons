39e84527d3b81138a50f639a68ee3de181958078
uwe-risse-sos <uwe.risse@sos-berlin.com>
2020-08-05 13:03:26 +0200

    JOC-993

--- sos-json/src/main/java/com/sos/joc/model/audit/AuditLogFilter.java
+++ sos-json/src/main/java/com/sos/joc/model/audit/AuditLogFilter.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.sos.joc.model.common.Folder;
-import com.sos.joc.model.jobstreams.JobStreamPath;
-
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
